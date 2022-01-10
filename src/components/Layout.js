937fc638b3b0518bd0715d54fd859e4f12e8ce5a
JNFussion <juan.jose.gutierrez.99@gmail.com>
2022-01-10 12:01:42 +0100

    remove tweets functions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,64 +1,10 @@
-import {
-  collection,
-  getFirestore,
-  limit,
-  onSnapshot,
-  orderBy,
-  query,
-} from "firebase/firestore";
 import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import ProfileBtn from "./ProfileBtn";
 import Tweet from "./Tweet";
 
-function Layout({ head }) {
-  const [tweets, setTweets] = useState([]);
-
-  // TWEETS RELATED
-
-  useEffect(() => {
-    function deleteTweet(id) {
-      setTweets((prevState) => prevState.filter((tweet) => tweet.id !== id));
-    }
-    function addTweet(id, timestamp, name, text, profilePicUrl, imageUrl) {
-      if (!tweets.find((tweet) => id === tweet.id)) {
-        setTweets((prevState) => [
-          ...prevState,
-          { id, timestamp, name, text, profilePicUrl, imageUrl },
-        ]);
-      }
-    }
-
-    function loadTweets() {
-      const recentTweetsQuery = query(
-        collection(getFirestore(), "tweets"),
-        orderBy("timestamp", "desc"),
-        limit(10)
-      );
-
-      onSnapshot(recentTweetsQuery, (snapshot) => {
-        snapshot.docChanges().forEach((change) => {
-          if (change.type === "removed") {
-            deleteTweet(change.doc.id);
-          } else {
-            const tweet = change.doc.data();
-            addTweet(
-              change.doc.id,
-              tweet.timestamp,
-              tweet.name,
-              tweet.text,
-              tweet.profilePicUrl,
-              tweet.imageUrl
-            );
-          }
-        });
-      });
-    }
-    loadTweets();
-    return () => {};
-  }, []);
-
+function Layout({ head, tweets }) {
   return (
     <article className="max-w-6xl mx-auto grid grid-cols-layout">
       <div>
@@ -88,6 +34,7 @@ function Layout({ head }) {
 
 Layout.propTypes = {
   head: PropTypes.element.isRequired,
+  tweets: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Layout;
